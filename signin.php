29ad3f488baa9f674e12ad7c0ca7c90b228b20ca
mohamadkalaoun <48067173+mohamadkalaoun@users.noreply.github.com>
2020-09-30 17:36:07 +0300

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,3 +1,17 @@
+<?php 
+include("login.php");
+      
+if(isset($_COOKIE["email"])) {
+         $encryption=$_COOKIE["email"]; 
+
+         $decryption1 = udecrypt($encryption);
+       }
+
+       if(isset($_COOKIE["password"])) {
+         $encryption2=$_COOKIE["password"]; 
+
+         $decryption2 = udecrypt($encryption2);
+       } ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,7 +19,8 @@
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="jquery.js"></script>
 </head>
@@ -62,14 +77,15 @@
 		        </div>
 		        <div class="l-part">
 		          <form  action="" method="post">
-		          	<input type="email" placeholder="Email" name="email" required class="form-control input-md" /><br>
+		          	<input type="email" placeholder="Email" name="email" required class="form-control input-md" value="<?php if(isset($_COOKIE["email"])) { echo "$decryption1";}?>" /><br>
 		          	<div class="overlap-text">
-			            <input type="password" placeholder="Password" name="pass" required class="form-control input-md" /><br>
+						<input type="password" placeholder="Password" name="pass" required class="form-control input-md" value="<?php if(isset($_COOKIE["password"])) { echo "$decryption2";}?>" /><br>
+						<p><input type="checkbox" name="remember" /> Remember me
 			            <a style="text-decoration:none; color:#187FAB;" data-toggle="tooltip" title="Reset Password"  href="forgot_password.php">Forgot?</a>
 		       		</div>
 		       		<a style="text-decoration:none;float: right; color:#187FAB;" data-toggle="tooltip" title="Create Account!" href="signup.php">Don't have an account?</a><br><br>
 		            <center><button id="signin" class="btn btn-info btn-lg" name="login">Login</button></center>
-		            <?php include("login.php"); ?>
+		            <?php // include("login.php"); ?>
 		          </form>
 		        </div>
 	      </div>
