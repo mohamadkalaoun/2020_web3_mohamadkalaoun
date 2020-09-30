5a7f5c202b34e38fc7ef01dc94a288465ab0cda6
mohamadkalaoun <48067173+mohamadkalaoun@users.noreply.github.com>
2020-09-30 17:08:43 +0300

    Update change_password.php

--- change_password.php
+++ change_password.php
@@ -16,7 +16,7 @@ if(!isset($_SESSION['user_email'])){
 <html>
 <head>
 	<title>Change Password</title>
-	<link rel="stylesheet" type="text/css" href="styles/forgot_password.css">
+	<link rel="stylesheet" type="text/css" href="style/forgot_password.css">
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
